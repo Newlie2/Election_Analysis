9537eb5dac00791eaa4c6774320065b7cda90822
Newlie2 <115044466+Newlie2@users.noreply.github.com>
2023-03-04 22:17:15 -0500

    updated files

--- Practice/Python_practice.py
+++ Practice/Python_practice.py
@@ -1,7 +1,32 @@
-# -*- coding: UTF-8 -*-
-"""PyPoll Homework Challenge Solution."""
 
-# Add our dependencies.
+
+
+
+# print("hello world")
+
+# counties = ["Arapahoe", "Denver", "Jefferson"]  
+# if counties == 0:
+#     print("Arapahoe")
+# if counties == 1:
+#     print("Denver")
+# if counties == 2:
+#     print("Jefferson")
+# len (counties)
+
+# counties.append("El paso")
+
+# print("counties")
+    
+    
+# counties_tuple =("Arapahoe", "Denver", "Jefferson")
+# len(counties_tuple)
+# 3
+
+# counties_tuple[1]
+# "Denver"
+
+
+#  Add our dependencies.
 import csv
 import os
 
